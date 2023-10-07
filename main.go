29651ab90dc023472e8d5570e54d2a2e9fa09228
Riad <riadafridishibly@gmail.com>
2023-10-08 00:24:11 +0600

    Add another example

--- main.go
+++ main.go
@@ -2,14 +2,86 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/riadafridishibly/proto-marshal-example/protoexample"
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func main() {
+func addressbookExample() {
+	person1 := &protoexample.Person{
+		Name:  "Jon Doe",
+		Id:    32,
+		Email: "jondoe@example.com",
+		Phones: []*protoexample.Person_PhoneNumber{
+			{
+				Number: "0123456",
+				Type:   protoexample.Person_HOME,
+			},
+			{
+				Number: "8976543",
+				Type:   protoexample.Person_WORK,
+			},
+		},
+		LastUpdated: timestamppb.New(time.Now()),
+	}
+	person2 := &protoexample.Person{
+		Name:  "Susan",
+		Id:    33,
+		Email: "susan@example.com",
+		Phones: []*protoexample.Person_PhoneNumber{
+			{
+				Number: "012345678",
+				Type:   protoexample.Person_HOME,
+			},
+			{
+				Number: "897654301",
+				Type:   protoexample.Person_WORK,
+			},
+		},
+		LastUpdated: timestamppb.New(time.Now().Add(20 * 24 * time.Hour)),
+	}
+	addrs := &protoexample.AddressBook{
+		People: []*protoexample.Person{
+			person1,
+			person2,
+		},
+	}
+	data := marshallDump("AddressBook", addrs)
+	var v protoexample.AddressBook
+	unmarshal("AddressBook", data, &v)
+}
+
+func marshallDump(name string, m proto.Message) []byte {
+	data, err := proto.Marshal(m)
+	if err != nil {
+		log.Fatal("Marshal failed:", err)
+	}
+	dump := hex.Dump(data)
+	log.Println("Proto Dump:", name)
+	fmt.Print(dump)
+	return data
+}
+
+func unmarshal(name string, data []byte, target proto.Message) {
+	log.Printf("Unmershalling %s...", name)
+	err := proto.Unmarshal(data, target)
+	if err != nil {
+		log.Fatalf("Faile: %s: err: %v", name, err)
+	}
+	jsonData, err := json.MarshalIndent(target, "", "  ")
+	if err != nil {
+		log.Fatal("Failed to marshal:", err)
+	}
+
+	log.Println("Result:\n", string(jsonData))
+}
+
+func rectExample() {
 	rect := &protoexample.Rectangle{
 		Lo: &protoexample.Point{
 			Latitude:  1,
@@ -21,20 +93,13 @@ func main() {
 		},
 	}
 
-	data, err := proto.Marshal(rect)
-	if err != nil {
-		log.Fatal("Marshal failed:", err)
-	}
-
-	dump := hex.Dump(data)
-	fmt.Println("Proto Dump:")
-	fmt.Println(dump)
+	data := marshallDump("Rect", rect)
 
 	var r protoexample.Rectangle
-	err = proto.Unmarshal(data, &r)
-	if err != nil {
-		log.Fatal("Unmarshal failed:", err)
-	}
+	unmarshal("Rect", data, &r)
+}
 
-	fmt.Println("Unmarshalled:", r.String())
+func main() {
+	rectExample()
+	addressbookExample()
 }
